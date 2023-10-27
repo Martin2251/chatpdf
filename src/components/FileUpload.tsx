726f3ee376739fcd9f1696c129c76248f2a20cb6
Martin Lowe <martinlowe673@gmail.com>
2023-10-27 07:36:26 +0100

    accept on file upload

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,15 +1,23 @@
 "use client"
+
+import { Inbox } from "lucide-react"
 import { useDropzone } from "react-dropzone"
 
 
 const FileUpload = () => {
-    const {getRootProps, getInputProps} = useDropzone()
+    const {getRootProps, getInputProps} = useDropzone({
+        accept:{"application/pdf":(".pg")}
+    })
   return (
 <div className="p-2 bg-white rounded-xl">
     <div {...getRootProps({
         className:"border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center "
     })}>
         <input  {...getInputProps()}/>
+        <>
+        <Inbox className="W-10 H-10 text-blue-500" />
+      <p className="mt-2 text-sm text-slate-400">Drop PDF here</p>
+        </>
     </div>
 </div>
   )
